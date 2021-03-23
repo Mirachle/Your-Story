29f47c1439794a3ad048030d91bb229f208ed231
Mirachle <cz.mirabella@freemail.hu>
2021-03-23 09:02:44 +0100

    add basic auth service

---
+++ src/services/Authentication/FakeAuthentication.ts
@@ -0,0 +1,41 @@
+import { Authentication } from '../Authentication';
+// TODO: save the state in case of f5-refresh
+class FakeAuthentication implements Authentication {
+  private static readonly SAVED_USER_NAMES_KEY: string = 'savedUserNames';
+  private storage: Storage;
+  private _isLoggedIn: boolean = false;
+  public isLoggedIn(): boolean {
+    return this._isLoggedIn
+  }
+
+  constructor(storage: Storage) {
+    this.storage = storage;
+  }
+
+  private readSavedUserNamesFromStorage(): Array<string> {
+    const rawSavedUserNames: string = this.storage.getItem(FakeAuthentication.SAVED_USER_NAMES_KEY);
+    if (!rawSavedUserNames) return [];
+    const savedUserNames: Array<string> = JSON.parse(rawSavedUserNames);
+    return savedUserNames;
+  }
+
+  public async login(username: string, password: string): Promise<void> {
+   const savedUserNames = this.readSavedUserNamesFromStorage();
+    const isMatch = savedUserNames.some(savedUserName => savedUserName === username);
+    if (!isMatch) {
+      throw new Error('Incorrect username or password.');
+    }
+    this._isLoggedIn = true;
+  }
+
+  public async register(username: string, _password: string): Promise<void> {
+    const savedUserNames = this.readSavedUserNamesFromStorage();
+    savedUserNames.push(username);
+    const stringifiedSavedUserNames = JSON.stringify(savedUserNames);
+    this.storage.setItem(FakeAuthentication.SAVED_USER_NAMES_KEY, stringifiedSavedUserNames);
+  }
+
+  public async logout(): Promise<void> {
+      this._isLoggedIn = false;
+  }
+}
