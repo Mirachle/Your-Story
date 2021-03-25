7dc7c04ef13b60a6fcc69f9a55eee2b5eb485627
Mirachle <cz.mirabella@freemail.hu>
2021-03-25 21:06:46 +0100

    add dummy Situation view

---
+++ src/components/Situation.vue
@@ -0,0 +1,22 @@
+<template>
+    <p>{{ situation.boy.name }}</p>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue';
+import { Situation } from '@/game/situation/Situation';
+
+export default Vue.extend({
+    props: {
+        situation: {
+            required: true,
+            type: Object as () => Situation
+        }
+    }
+})
+</script>
+
+<style>
+
+</style>
