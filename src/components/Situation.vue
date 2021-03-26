02581844b2c7022be129300209ef11e4b7bd75fd
Mirachle <cz.mirabella@freemail.hu>
2021-03-26 22:31:10 +0100

    WIP: situation view

--- src/components/Situation.vue
+++ src/components/Situation.vue
@@ -1,11 +1,18 @@
 <template>
-    <p>{{ situation.boy.name }}</p>
+    <div :style="{ backgroundImage: sceneBackground }" class="bg-image">
+        <div class="speech">{{ situation.dialogue.name }}: {{ situation.dialogue.text }}</div>
+        <img v-if="situation.boy" id="boy-image" :src="boyImage">
+        <div class="rows-container">
+            <div @click="choseAnswer(answer)" class="answer-row" v-for="(answer, index) in situation.dialogue.answers" :key="index">{{ answer.text }}</div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
-
+// TODO: create Game class, so its state can be saved (Memento pattern), and it can collect all points
 import Vue from 'vue';
 import { Situation } from '@/game/situation/Situation';
+import { Answer } from '@/game/situation/Answer';
 
 export default Vue.extend({
     props: {
@@ -13,10 +20,106 @@ export default Vue.extend({
             required: true,
             type: Object as () => Situation
         }
+    },
+    computed: {
+        sceneBackgroundImage() {
+            return `url(${require(this.situation.scene.image)})`;
+        },
+        boyImage() {
+            const boyName = this.situation.boy.name;
+            const mood = this.situation.boy.mood;
+            // TODO: do this shit;
+            // https://stackoverflow.com/questions/42118296/dynamically-import-images-from-a-directory-using-webpack
+            return require(`@/assets/${boyName}/${boyName}_${mood}.png`);
+        }
+    },
+    methods: {
+        choseAnswer(answer: Answer) {
+            this.$emit('select-next-situation', answer.followingSituation);
+        }
     }
 })
 </script>
 
 <style>
+#boy-image {
+    width: auto;
+    max-height: 100%;
+    position: absolute;
+    bottom: 35px;
+    max-width: 50vw;
+}
 
+.rows-container {
+    position: absolute;
+    font-size: large;
+    bottom: 0px;
+    width: 100%;
+}
+
+.answer-row {
+    background-color: white;
+    width: 100%;
+    color: black;
+    padding: 5px;
+    border: 1px solid #ff8ddc;
+}
+
+.answer-row:hover {
+    background-color: #ffddf2;;
+}
+
+
+.speech {
+    font-size: x-large;
+    color: black;
+    position: absolute;
+    background: white;
+    padding: 10px;
+    border: 2px solid black;
+    border-radius: 10px;
+    max-width: 320px;
+    top: 20%;
+    right: 10%;
+}
+
+.bg-image {
+    background-image: url('../assets/scenes/pub.jpg');
+    width: auto;
+    height: 100%;
+    background-size: contain;
+    background-position: center center;
+    background-repeat: no-repeat;
+    position: relative;
+    text-align: left;
+}
+
+
+@media only screen and (max-width: 991.9px) {
+   .bg-image {
+        width: auto;
+        height: 70vh;
+    }
+    .speech {
+        top: 33%;
+        right: 12%;
+        font-size: small;
+    }
+    #boy-image {
+        max-height: 70%;
+    }
+}
+
+@media only screen and (max-width: 600px) {
+   .bg-image {
+        width: auto;
+        height: 50vh;
+    }
+    .speech {
+        top: 33%;
+        right: 1%;
+        font-size: xx-small;
+
+    }
+}
 </style>
