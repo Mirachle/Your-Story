7056d5a0dbc3781d5fa08ee635aaa868fc0e5566
Mirachle <cz.mirabella@freemail.hu>
2021-03-26 22:30:22 +0100

    add dummy trees and propagate it to parent

--- src/components/BoySelector.vue
+++ src/components/BoySelector.vue
@@ -2,14 +2,14 @@
     <div>
         <h1>Válaszd ki melyik fiúval szeretnél elmenni randevúra.</h1>
         <div class="bottom">
-            <div class="col-md-4 center box" @click="selectBoy('béla')">
-                <img class="img-responsive" src="@/assets/béla/béla_normal.png" />
+            <div class="col-md-4 center box" @click="selectBoy('bela')">
+                <img class="img-responsive" src="@/assets/bela/bela_normal.png" />
             </div>
             <div class="col-md-4 center box" @click="selectBoy('athem')">
                 <img class="img-responsive" src="@/assets/athem/athem_normal.png" />
             </div>
-            <div class="col-md-4 center box" @click="selectBoy('ágoston')">
-                <img class="img-responsive" src="@/assets/ágoston/ágoston_normal.png" />
+            <div class="col-md-4 center box" @click="selectBoy('agoston')">
+                <img class="img-responsive" src="@/assets/agoston/agoston_normal.png" />
             </div>
         </div>
     </div>
@@ -20,22 +20,27 @@ import Vue from 'vue'
 import { SituationTreeFactory } from '@/game/situation/trees/SituationTreeFactory';
 import { athemTree } from '@/game/situation/trees/Athem';
 import { RawSituation } from '@/game/situation/trees/RawSituation';
+import { belaTree } from '../game/situation/trees/Bela';
+import { agostonTree } from '../game/situation/trees/Agoston';
 
 export default Vue.extend({
     name: 'BoySelector',
     methods: {
-        selectBoy(boyName: 'béla' | 'athem' | 'ágoston') {
+        selectBoy(boyName: 'bela' | 'athem' | 'agoston') {
             let rawSituations: Array<RawSituation>;
             switch(boyName) {
                 case 'athem':
                     rawSituations = athemTree;
-                default:
-                    console.warn('there is no implemented boy for this tree, using Athem');
-                    boyName = 'athem';
-                    rawSituations = athemTree;
+                    break;
+                case 'bela':
+                    rawSituations = belaTree;
+                    break;
+                case 'agoston':
+                    rawSituations = agostonTree;
+                    break;
             }
-            const athemProcessedTree = new SituationTreeFactory().createSituationTree(boyName, rawSituations);
-            this.$emit('select-situation', athemProcessedTree);
+            const processedTree = new SituationTreeFactory().createSituationTree(boyName, rawSituations);
+            this.$emit('select-situation', processedTree);
         }
     }
 })
