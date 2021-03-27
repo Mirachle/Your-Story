b852d6e8059d55516cdd0226bf5e83bc4c3cc323
Mirachle <cz.mirabella@freemail.hu>
2021-03-27 12:19:14 +0100

    add TextFormatter and its tests

---
+++ src/game/formatter/TextFormatter.ts
@@ -0,0 +1,28 @@
+import { Logger } from '@/Logger';
+
+export class TextFormatter {
+    private static readonly TEMPLATE_REGEXP = /\@\{(\w+)\}/g;
+    private static readonly FALLBACK_VALUE = '...';
+
+    private definedVariableNames: Record<string, string>;
+    private logger: Logger;
+
+    constructor(definedVariableNames: Record<string, string>, logger: Logger) {
+        this.definedVariableNames = definedVariableNames;
+        this.logger = logger;
+    }
+
+    private replaceTemplateToText = function(template: string, variableName: string): string {
+        const storedValue = this.definedVariableNames[variableName];
+        if (!storedValue) {
+            this.logger.warn(`could not replace template ${template}, as there is no stored value for it`);
+            return TextFormatter.FALLBACK_VALUE;
+        } else {
+            return storedValue;
+        }
+    }.bind(this);
+
+    public formateTemplateText(text: string): string {
+        return text.replace(TextFormatter.TEMPLATE_REGEXP, this.replaceTemplateToText);
+    }
+}
---
+++ src/game/formatter/__tests__/TextFormatter.spec.ts
@@ -0,0 +1,55 @@
+import { TextFormatter } from '../TextFormatter';
+import { Logger } from '../../../Logger';
+
+const noop = (..._args: any[]) => {};
+
+class MockLogger implements Logger {
+    public warn = jest.fn((_message: string) => {});
+    error = noop;
+    info = noop;
+    log = noop;
+    trace = noop;
+}
+
+describe('TextFormatter', () => {
+    const storedValues = {
+        username: 'MiraBela',
+        chosenSubject: 'fakanál'
+    };
+
+    let unitUnderTest: TextFormatter;
+    let mockLogger: MockLogger;
+
+    beforeEach(() => {
+        mockLogger = new MockLogger();
+        unitUnderTest = new TextFormatter(storedValues, mockLogger);
+    });
+
+    it('should do nothing with templateless text ', () => {
+        const templatelessText = 'Hello, Athem vagyok!';
+        const actualValue = unitUnderTest.formateTemplateText(templatelessText);
+        expect(actualValue).toEqual(templatelessText);
+        expect(mockLogger.warn).not.toHaveBeenCalled();
+    });
+
+    it('should replace one templatePart with proper value', () => {
+        const oneTemplatePartText = `Szia, @{username} vagyok`;
+        const actualValue = unitUnderTest.formateTemplateText(oneTemplatePartText);
+        expect(actualValue).toEqual(`Szia, MiraBela vagyok`);
+        expect(mockLogger.warn).not.toHaveBeenCalled();
+    });
+
+    it('should replace two templateParts with proper values', () => {
+        const twoTemplatePartText = `@{username} azt mondta, hogy nincs nálad @{chosenSubject}`;
+        const actualValue = unitUnderTest.formateTemplateText(twoTemplatePartText);
+        expect(actualValue).toEqual(`MiraBela azt mondta, hogy nincs nálad fakanál`);
+        expect(mockLogger.warn).not.toHaveBeenCalled();
+    });
+
+    it('should replace templatePart with fallback value and log lack of value', () => {
+        const templatePartText = `Nem találom a @{notFoundSubject}-ot`;
+        const actualValue = unitUnderTest.formateTemplateText(templatePartText);
+        expect(actualValue).toEqual(`Nem találom a ...-ot`);
+        expect(mockLogger.warn).toHaveBeenCalled();
+    });
+});
